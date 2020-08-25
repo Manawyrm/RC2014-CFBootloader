bb2aa6aa60c59ed309f618383ed2ca82dfe0b86e
Tobias Mädel <t.maedel@alfeld.de>
2020-08-25 16:49:23 +0200

    initial support for SIO/2, loses bytes at TX, DO NOT USE

--- uart/uart.c
+++ uart/uart.c
@@ -1,22 +1,24 @@
 #include "uart.h"
 #include <string.h>
+#include <z80.h>
 
-__sfr __at 0x80 ACIA_CONTROL;
-__sfr __at 0x81 ACIA_DATA;
+volatile __sfr __at 0x80 SIOA_REGISTER;
+volatile __sfr __at 0x81 SIOA_DATA; 
+volatile __sfr __at 0x82 SIOB_REGISTER;
+volatile __sfr __at 0x83 SIOB_DATA;
+ 
 
 #define UART_RX_SIZE 128
 uint8_t uart_rx_buffer[UART_RX_SIZE];
 volatile uint8_t rxhead, rxtail;
 
-char uart_register_read (void)
-{
-  while ((ACIA_CONTROL & 1) == 0) {};
-  return ACIA_DATA;
-}
-
 void z80_rst_38h(void) __critical __interrupt(0)
 {
-  uart_rx_buffer[rxtail] = uart_register_read();
+  SIOA_REGISTER = 0x00; 
+  if (!(SIOA_REGISTER & 0x01))
+    return; 
+
+  uart_rx_buffer[rxtail] = SIOA_DATA;
   rxtail++;
   rxtail &= 127;
 
@@ -25,7 +27,7 @@ void z80_rst_38h(void) __critical __interrupt(0)
 
 uint8_t uart_available()
 {
-    return rxhead ^ rxtail;
+  return rxhead ^ rxtail;
 }
 
 uint8_t uart_read()
@@ -39,16 +41,23 @@ uint8_t uart_read()
    return key;
 }
 
-inline char uart_rx_filled()
+inline void uart_a_set_register(uint8_t reg, uint8_t data)
 {
-    return (ACIA_CONTROL & 1);
+  SIOA_REGISTER = reg;
+  SIOA_REGISTER = data;
 }
 
 
 void uart_init(void)
 {
-  ACIA_CONTROL = 0x96; // interrupt 
-}
+  SIOA_REGISTER = 0x18; // WR0, Channel Reset 
+  z80_delay_ms(10);
+
+  uart_a_set_register(0x04, 0xC4); // WR4, x64 Clock Devide
+  uart_a_set_register(0x03, 0xC1); // WR3, RX Enabled
+  uart_a_set_register(0x05, 0xE8); // WR5, 8bit Mode, TX Enabled (Bit 3)
+  uart_a_set_register(0x01, 0x18); // WR1, INT on All Rx Characters (without Parity)
+} 
 
 
 void putstring_uart(uint8_t *text)
@@ -62,7 +71,11 @@ void putstring_uart(uint8_t *text)
 
 void putchar_uart (char tx_data)
 {
-    // write to registers to tx the data. TX buffering, etc
-    while ((ACIA_CONTROL & 2) == 0) {};
-	ACIA_DATA = tx_data;
+  SIOA_REGISTER = 0x01; 
+  while (SIOA_REGISTER & 0x01 == 0)
+  {
+    SIOA_REGISTER = 0x01;
+  }
+  
+	SIOA_DATA = tx_data;
 }
--- uart/uart.h
+++ uart/uart.h
@@ -3,8 +3,6 @@
 
 void putchar_uart (char tx_data);
 void putstring_uart(uint8_t *text);
-char uart_register_read (void);
 void uart_init(void);
-char uart_rx_filled();
 uint8_t uart_available();
 uint8_t uart_read();
